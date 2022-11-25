327b95d88bef564ce8a7fa9275ee6bc34209ad42
N V Sai Likhith <nvsailikhith@gmail.com>
2022-11-25 10:30:56 +0530

    Update FailingTC.py

--- FailingTC.py
+++ FailingTC.py
@@ -1,22 +1,23 @@
+#!/usr/bin/python3
 import unittest
 
 from Palindrom import isPalindrome
 
 class Tests(unittest.TestCase): 
   
-  def failingTC1(self):
+  def testcasefailing1(self):
     string = "madam"
     trueResult = False
     testResult = isPalindrome(string)
     self.assertEqual(testResult, trueResult)
   
-  def failingTC1(self):
+  def testcasefailing2(self):
     string = "software-engineering"
     trueResult = True
     testResult = isPalindrome(string)
     self.assertEqual(testResult, trueResult)
     
-  def failingTC1(self):
+  def testcasefailing3(self):
     string = ""
     trueResult = False
     testResult = isPalindrome(string)
