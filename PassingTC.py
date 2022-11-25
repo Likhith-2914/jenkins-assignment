b76fd01cf488c538c2a87b8ebe05227550ab7f30
N V Sai Likhith <nvsailikhith@gmail.com>
2022-11-25 10:29:46 +0530

    Update PassingTC.py

--- PassingTC.py
+++ PassingTC.py
@@ -4,19 +4,19 @@ from Palindrom import isPalindrome
 
 class Tests(unittest.TestCase): 
   
-  def passingTC1(self):
+  def testcasepassing1(self):
     string = "rececar"
     trueResult = True
     testResult = isPalindrome(string)
     self.assertEqual(testResult, trueResult)
   
-  def passingTC1(self):
+  def testcasepassing2(self):
     string = "likhith"
     trueResult = False
     testResult = isPalindrome(string)
     self.assertEqual(testResult, trueResult)
     
-  def passingTC1(self):
+  def testpassing3(self):
     string = "a12421a"
     trueResult = True
     testResult = isPalindrome(string)
